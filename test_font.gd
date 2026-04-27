dc417fff1a6bf48cf89985b161dc6594d0c844fd
gracetyy <145827531+gracetyy@users.noreply.github.com>
2026-04-28 00:24:52 +0800

    chore: remove useless files

--- test_font.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-
-func _init():
-	var font = load("res://assets/fonts/Fredoka-Variable.ttf")
-	var size = font.get_multiline_string_size("This is a visited tile.", 1, 167, 16)
-	print("SIZE: ", size)
-	quit()
